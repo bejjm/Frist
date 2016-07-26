c72371d992796449053819abf194f3e375173515
Jiabin Luo <1250666351@qq.com>
2016-07-26 14:55:41 +0800

    second test git!

---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test {
+	int a;
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		new Test().go();
+	}
+
+	public void go() {
+		int b;
+		System.out.println();
+		System.out.println(a);
+	}
+	String g="git test";
+}
