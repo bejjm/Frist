26a0024a8a3b3f3992afcbf1d7b06212a78f43cf
Jiabin Luo <1250666351@qq.com>
2016-07-26 15:11:27 +0800

    update src!

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,16 +2,14 @@ package test;
 
 public class Test {
 	int a;
-
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		new Test().go();
 	}
-
+String gg="sss";
 	public void go() {
 		int b;
 		System.out.println();
 		System.out.println(a);
 	}
-	String g="git test";
 }
--- src/test02/GitTest.java
+++ src/test02/GitTest.java
@@ -4,7 +4,5 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String b="git test02";
 	}
-
 }
